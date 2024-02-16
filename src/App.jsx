2ba8c57ff1a92147d31100c9a25201724933ef25
Prathamesh Jadhav <prathameshj610@gmail.com>
2024-02-17 00:50:43 +0530

    added maintenance banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Navbar } from './components/Navbar'
-import { Home } from './components/Home'
-import { Footer } from './components/Footer'
+// import { Navbar } from './components/Navbar'
+// import { Home } from './components/Home'
+// import { Footer } from './components/Footer'
+import { MaintenanceBanner } from './components/MaintenanceBanner'
 
 function App() {
   return (<>
     <div className='App'>
-      <Navbar />
+      <MaintenanceBanner />
+      {/* <Navbar />
       <Home />
-      <Footer /> 
+      <Footer />  */}
     </div>
     </>
   )
