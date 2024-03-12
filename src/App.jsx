e2d0d4e7bdfcdf99aa162b42f5ab9ea3d4f013f5
Prathamesh Jadhav <39017958+prathamesh610@users.noreply.github.com>
2024-03-12 11:25:13 +0530

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-// import { Navbar } from './components/Navbar'
-// import { Home } from './components/Home'
-// import { Footer } from './components/Footer'
-import { MaintenanceBanner } from './components/MaintenanceBanner'
+import { Navbar } from './components/Navbar'
+import { Home } from './components/Home'
+import { Footer } from './components/Footer'
+// import { MaintenanceBanner } from './components/MaintenanceBanner'
 
 function App() {
   return (<>
